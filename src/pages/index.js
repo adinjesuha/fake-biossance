83e6da30e528e1f5ae09a5aae8f5281589665a13
vagrant <vagrant@vagrant.vm>
2021-09-27 04:00:10 +0000

    add commit from vagrant

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Layout from "gatsby-theme-blog/src/components/layout"
+
+const Home = (props) => (
+  <Layout title="Home" {...props}>
+    <h1>Home page</h1>
+  </Layout>
+)
+
+export default Home
